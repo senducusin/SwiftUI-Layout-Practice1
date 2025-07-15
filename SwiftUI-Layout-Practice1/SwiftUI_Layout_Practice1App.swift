0e960e055e8e9088b5acf9423b3ad3712d5c1f74
Jansen Ducusin <senducusin@gmail.com>
2025-07-16 01:47:29 +0800

    Initial Commit

---
+++ SwiftUI-Layout-Practice1/SwiftUI_Layout_Practice1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Layout_Practice1App.swift
+//  SwiftUI-Layout-Practice1
+//
+//  Created by Jansen Ducusin on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Layout_Practice1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
